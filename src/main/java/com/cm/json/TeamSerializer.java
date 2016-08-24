3b81718f15dbb3fadb59020c610b9ecb57e358c6
Voidbezdagon <voidbezdagon@gmail.com>
2016-08-24 17:01:36 +0300

    Schedule Serializing Fixes

--- src/main/java/com/cm/json/TeamSerializer.java
+++ src/main/java/com/cm/json/TeamSerializer.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.cm.entity.Position;
 import com.cm.entity.Team;
+import com.cm.entity.User;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -15,7 +17,28 @@ public class TeamSerializer extends JsonSerializer<Team>{
 	@Override
 	public void serialize(Team arg0, JsonGenerator arg1, SerializerProvider arg2)
 			throws IOException, JsonProcessingException {
-	        final SimpleTeam list = new SimpleTeam(arg0.getId(), arg0.getTeamname());
+			ArrayList<User> users = new ArrayList<User>();
+			for (User u: arg0.getUsers())
+			{
+				User user = new User();
+				user.setAccesskey(u.getAccesskey());
+				user.setAdmin(u.getAdmin());
+				user.setAvatar(u.getAvatar());
+				user.setFirstname(u.getFirstname());
+				user.setId(u.getId());
+				user.setLastname(u.getLastname());
+				user.setPassword(u.getPassword());
+				user.setUsername(u.getUsername());
+				//Setting Position
+					Position p = new Position();
+					p.setId(u.getPosition().getId());
+					p.setLevel(u.getPosition().getLevel());
+					p.setName(u.getPosition().getName());
+					p.setParentId(u.getPosition().getParentId());
+				user.setPosition(p);
+				users.add(user);
+			}
+	        final SimpleTeam list = new SimpleTeam(arg0.getId(), arg0.getTeamname(), users);
 	        arg1.writeObject(list);
 	}
 	
@@ -23,11 +46,13 @@ public class TeamSerializer extends JsonSerializer<Team>{
 	{
 		private Long id;
 		private String teamname;
-
-		public SimpleTeam(Long id, String tn)
+		private List<User> users;
+		
+		public SimpleTeam(Long id, String tn, List<User> users)
 		{
 			this.id = id;
 			this.teamname = tn;
+			this.users = users;
 		}
 		
 		public Long getId() {
@@ -44,6 +69,14 @@ public class TeamSerializer extends JsonSerializer<Team>{
 
 		public void setTeamname(String teamname) {
 			this.teamname = teamname;
+		}
+
+		public List<User> getUsers() {
+			return users;
+		}
+
+		public void setUsers(List<User> users) {
+			this.users = users;
 		}
 	}
 
